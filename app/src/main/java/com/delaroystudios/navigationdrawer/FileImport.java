232306b0aac40b2a4d04fbfcda4c5c31d0dd79e0
phuongnam0907 <phuongnam0907@gmail.com>
2019-04-19 15:55:20 +0700

    update

--- app/src/main/java/com/delaroystudios/navigationdrawer/FileImport.java
+++ app/src/main/java/com/delaroystudios/navigationdrawer/FileImport.java
@@ -7,6 +7,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -29,6 +31,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import de.blox.treeview.BaseTreeAdapter;
+import de.blox.treeview.TreeNode;
+import de.blox.treeview.TreeView;
 
 /**
  * Created by delaroy on 3/18/17.
@@ -44,7 +52,16 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
     boolean isRunning = false;
     Handler mHandler = new Handler();
 
+    TreeNode rootNode;
+    BaseTreeAdapter adapter;
+
     private TextView mTextViewResult;
+
+    private ArrayList<String> stringArrayList;
+    private ArrayList<TreeNode> treeNodeArrayList;
+
+    final String urlmap = "http://192.168.97.1/rpi3/backend/getmap.php";
+    final String urlgw = "http://192.168.97.1/rpi3/backend/getgwnode.php";
 
 
     @Override
@@ -54,7 +71,42 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
         //Node Mapping Activity
         init();
 
-        mTextViewResult = (TextView) findViewById(R.id.node);
+        //mTextViewResult = (TextView) findViewById(R.id.node);
+
+
+
+        TreeView treeView = findViewById(R.id.tree);
+
+        adapter = new BaseTreeAdapter<ViewHolder>(this, R.layout.node) {
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(View view) {
+                return new ViewHolder(view);
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder viewHolder, Object data, int position) {
+                viewHolder.mTextView.setText(data.toString());
+            }
+        };
+        treeView.setAdapter(adapter);
+
+        // example tree
+        rootNode = new TreeNode("  Server  ");
+        rootNode.addChild(new TreeNode("  Node 1  "));
+        final TreeNode child3 = new TreeNode("  Node 2  ");
+        child3.addChild(new TreeNode("  Node 3  "));
+        final TreeNode child6 = new TreeNode("  Node 4  ");
+        child6.addChild(new TreeNode("  Node 5  "));
+        child6.addChild(new TreeNode("  Node 6  "));
+        child3.addChild(child6);
+        rootNode.addChild(child3);
+        final TreeNode child4 = new TreeNode("  Node 7  ");
+        child4.addChild(new TreeNode("  Node 8  "));
+        child4.addChild(new TreeNode("  Node 9  "));
+        rootNode.addChild(child4);
+
+        adapter.setRootNode(rootNode);
 
 
     }
@@ -63,7 +115,8 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
         public void run() {
             try {
                 isRunning = true;
-                jsonParse();
+                getGateway();
+                //jsonParse();
                 Log.d("Times","0");
                 mHandler.postDelayed(loopR, 60000);
             } catch (Exception e) {
@@ -72,6 +125,40 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
             }
         }
     };
+
+    private void getGateway() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlgw,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonarray = new JSONArray(response);
+                            stringArrayList = new ArrayList<String>();
+                            rootNode = new TreeNode("  Server  ");
+                            for (int i = 0; i < jsonarray.length(); i++) {
+
+                                JSONObject jsonobj = jsonarray.getJSONObject(i);
+
+                                TreeNode child = new TreeNode("  Gateway " + jsonobj.getString("gateway") + "  ");
+                                if (jsonobj.getString("son").equals("1")) jsonParse(child, jsonobj.getString("gateway"));
+                                rootNode.addChild(child);
+                                stringArrayList.add(jsonobj.getString("gateway"));
+                            }
+                            Log.d("length", String.valueOf(stringArrayList.size()));
+                            adapter.setRootNode(rootNode);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        queue.add(stringRequest);
+    }
 
 
     private void init() {
@@ -175,32 +262,20 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
         return true;
     }
 
-    private void jsonParse() {
-
-        String url = "http://192.168.97.1/rpi3/backend/getmap.php";
+    private void jsonParse(final TreeNode treeNode1, final String gateway1) {
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
         final StringBuilder s = new StringBuilder();
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlmap,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             JSONArray jsonarray = new JSONArray(response);
+                            loopFor(jsonarray,0,gateway1,treeNode1);
 
-                            for (int i = 0; i < jsonarray.length(); i++) {
-
-                                JSONObject jsonobj = jsonarray.getJSONObject(i);
-
-                                s.append("ip : " + i + " = " + jsonobj.getString("ip") + "\n");
-                            }
-
-
-                            mTextViewResult.setText(s.toString());
-                            Log.d("Str",s.toString());
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -208,12 +283,38 @@ public class FileImport extends AppCompatActivity implements NavigationView.OnNa
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextViewResult.setText("That didn't work!");
             }
         });
-        // Add the request to the RequestQueue.
         queue.add(stringRequest);
 
     }
 
+    private class ViewHolder {
+        TextView mTextView;
+        ViewHolder(View view) {
+            mTextView = view.findViewById(R.id.textView);
+        }
+    }
+
+    private void loopFor(JSONArray jsonArray, int position, String gateway, TreeNode treeNode){
+        try {
+
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                //TreeNode child = new TreeNode();
+                if ((gateway.equals(jsonObject.getString("dad"))) && (jsonObject.getString("son").equals("1"))) {
+                    TreeNode child = new TreeNode("  Node " + jsonObject.getString("ip") + "  ");
+                    loopFor(jsonArray, position + 1, jsonObject.getString("ip"), child);
+//                    treeNode.addChild(loopFor(jsonArray, position + 1, jsonObject.getString("ip")));
+                    treeNode.addChild(child);
+
+                } else if ((gateway.equals(jsonObject.getString("dad"))) && (jsonObject.getString("son").equals("0"))){
+//                    child = new TreeNode("  Node " + jsonObject.getString("ip") + "  ");
+                    treeNode.addChild(new TreeNode("  Node " + jsonObject.getString("ip") + "  "));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
